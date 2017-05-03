da518c156bad1ea50aa3e0654ded1a23f95bf5be
Dian Tai <diantai@gmail.com>
2017-05-03 18:57:26 +0000

    Lesson 41: Previewing the Automated Email

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,8 @@ class NotificationMailer < ApplicationMailer
   default from: 'no-reply@nomster-dian-tai.herokuapp.com'
 
   def comment_added
-    mail(to: "diantai@gmail.com", subject: 'A comment has been added to your place')
+    mail(to: "dtqonn@gmail.com",
+    subject: "A comment has been added to your place")
   end
 
 end
